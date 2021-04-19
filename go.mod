caa416a99aac3d537ac84551287ca9ee1b82d1a1
ghokun <gokhun@gmail.com>
2021-04-19 12:40:35 +0300

    Added go-tty for testing
    Signed-off-by: ghokun <gokhun@gmail.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ghokun/termdim
 
 go 1.16
 
-require golang.org/x/sys v0.0.0-20210415045647-66c3f260301c
+require (
+	github.com/mattn/go-tty v0.0.3
+	golang.org/x/sys v0.0.0-20210415045647-66c3f260301c
+)
--- termdim_test.go
+++ termdim_test.go
@@ -2,12 +2,18 @@ package termdim
 
 import (
 	"fmt"
-	"os"
 	"testing"
+
+	"github.com/mattn/go-tty"
 )
 
 func TestGetSize(t *testing.T) {
-	width, height, err := GetSize(int(os.Stdout.Fd()))
+	tty, err := tty.Open()
+	if err != nil {
+		t.Fatal(err)
+	}
+	tty.Output().Fd()
+	width, height, err := GetSize(int(tty.Output().Fd()))
 	if err != nil {
 		t.Fatal(err)
 	}
