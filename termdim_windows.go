880774b0149e1e3dda674e7bf29b578bd8ade41a
ghokun <gokhun@gmail.com>
2021-04-19 12:53:06 +0300

    Fixing test errors
    Signed-off-by: ghokun <gokhun@gmail.com>

--- termdim_windows.go
+++ termdim_windows.go
@@ -11,5 +11,5 @@ func getSize(fd int) (width, height int, err error) {
 	if err := windows.GetConsoleScreenBufferInfo(windows.Handle(fd), &info); err != nil {
 		return -1, -1, err
 	}
-	return info.Window.Right - info.Window.Left + 1, info.Window.Bottom - info.Window.Top + 1, nil
+	return int(info.Window.Right - info.Window.Left + 1), int(info.Window.Bottom - info.Window.Top + 1), nil
 }
