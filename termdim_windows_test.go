bc71281bdf290d3d633b89001231e29c304a5a78
ghokun <gokhun@gmail.com>
2021-04-19 14:02:03 +0300

    Fixing test errors
    Signed-off-by: ghokun <gokhun@gmail.com>

--- termdim_windows_test.go
+++ termdim_windows_test.go
@@ -13,10 +13,20 @@ func TestGetSizeWindows(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	width, height, err := GetSize(int(tty.Fd()))
+	winsize := <-tty.SIGWINCH()
+	expectedWidth := winsize.W
+	expectedHeight := winsize.H
+
+	width, height, err := GetSize(int(tty.Output().Fd()))
 	if err != nil {
 		t.Fatal(err)
 	}
+	if width != expectedWidth {
+		t.Fatalf("%d != %d", width, expectedWidth)
+	}
 	t.Logf("Terminal width: %d", width)
+	if height != expectedHeight {
+		t.Fatalf("%d != %d", height, expectedHeight)
+	}
 	t.Logf("Terminal height: %d", height)
 }
